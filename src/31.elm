c1abb18fa85b70c4a1d54ea87cf3063b2a21153c
Scott Moore <scott.will.moore@gmail.com>
2018-02-28 14:05:16 +1100

    complete problems 28a and 31

---
+++ src/31.elm
@@ -0,0 +1,58 @@
+module Main exposing (..)
+
+import Html
+
+
+isPrime : Int -> Bool
+isPrime n =
+    if n < 2 then
+        False
+    else
+        eratosthenes (abs n) (List.range 2 (n // 2))
+
+
+eratosthenes : Int -> List Int -> Bool
+eratosthenes n sieve =
+    case sieve of
+        [] ->
+            True
+
+        x :: xs ->
+            if n % x == 0 then
+                False
+            else
+                eratosthenes n (List.filter (\i -> i % x /= 0) xs)
+
+
+main : Html.Html a
+main =
+    Html.text <|
+        case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length <|
+        List.filter (\( result, expect ) -> result /= expect)
+            [ ( isPrime 36, False )
+            , ( isPrime 10, False )
+            , ( isPrime -1, False )
+            , ( isPrime 1, False )
+            , ( isPrime 0, False )
+            , ( isPrime 120, False )
+            , ( isPrime 2, True )
+            , ( isPrime 23, True )
+            , ( isPrime 6000, False )
+            , ( isPrime 7919, True )
+            , ( isPrime 7911, False )
+            , ( isPrime 63247, True )
+            , ( isPrime 63249, False )
+            ]
